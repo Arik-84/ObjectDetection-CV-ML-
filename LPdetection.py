e50139d256176893ebc5e5d3d5a1c9453123774e
Arik-84 <adhali69@uwo.ca>
2025-05-31 12:32:07 -0400

    Update LPdetection.py

--- LPdetection.py
+++ LPdetection.py
@@ -3,13 +3,13 @@ from ultralytics import YOLO
 from PIL import Image
 import numpy as np
 
-# Loading Fine Tuned model
+# Load your trained model
 model = YOLO("best2.pt")
 
 st.title("License Plate Detection")
 
 
-# File uploader
+# File upload
 uploaded_file = st.file_uploader("Upload an image of car/cars and I will find your license plate!!", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
@@ -29,19 +29,19 @@ if uploaded_file is not None:
 st.subheader("Model Summary")
 
 st.markdown("Model transfer learned off of YOLOv8/fine tuned for darkness in object detection and trained on 7K images from RoboFlow. ")
-st.markdown("First Train: Transfer learned off of YOLOv8 model with 7k training images and 2k for validation. ")
-st.markdown("Fine Tuning: Used openCV for augmentation to account for low-light conditions.")
+st.markdown("**First Train**: Transfer learned off of YOLOv8 model with 7k training images and 2k for validation. ")
+st.markdown("**Fine Tuning**: Used openCV for augmentation to account for low-light conditions and trained on a subset of 2k images from the original training set. ")
 st.markdown("""
 **Evaluation Metrics**
-| Version      | Box Precision | Recall | mAP@0.5 | mAP@0.5:0.95 | Notes |
-|--------------|---------------|--------|---------|--------------|-------|
-| First Train  | 0.985         | 0.942  | 0.971   | 0.707        | Trained on 7K images for 25 epochs|
-| Fine-tuned   | *TBD*         | *TBD*  | *TBD*   | *TBD*        | Will improve low-light detection |
+| Version      | Box Precision | Recall | mAP@0.5 | Notes |
+|--------------|---------------|--------|---------|-------|
+| First Train  | 0.985         | 0.942  | 0.971   | Trained on 7K images for 25 epochs|
+| Fine-tuned   | 0.981         | 0.933  | 0.970   | Improved low-light detection |
 """)
 
 st.subheader("Improvements")
-st.markdown("1. **Cloud**: The model took about 55 hours in total training. Next time, I would use a cloud service like AWS to speed up training.")
+st.markdown("1. **Cloud**: The model took about 55 hours in total training. Next time, I would use a cloud service like AWS EC2 or SageMaker to speed up training.")
 st.markdown("2. **Data**: I would have liked to used more data on low light conditions outside of my augmnetation of 2000 images from the orginal training set, sourced more data for obstructed plates and created a full sized unseen testing dataset.")
-st.markdown("3. **Next Step**: With given time I would add a plate reading feature to this project. After detecting a licesne plate, tell what the plate number is.")
+st.markdown("3. **Next Step**: With given time I would add a plate reading feature to this project. After detecting a licesne plate, tell me what the plate number is...")
 
-st.subheader("For Avolta by Arik")
+st.markdown("###### For Avolta by Arik")
